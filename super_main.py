4864dfb725c4452250a5678f104b4efcf79c531a
beer <beer5214@126.com>
2022-07-23 10:44:00 +0800

    support task_file param

--- super_main.py
+++ super_main.py
@@ -3,8 +3,12 @@ import sys
 import os
 
 
-def main(base_dir: str):
-    with open(os.path.join(base_dir, 'task.txt'), 'r') as f:
+def main(base_dir: str, task_txt_file: str):
+    final_path = os.path.join(base_dir, task_txt_file)
+    if not os.path.exists(final_path):
+        print('final_path: %s, not exists'.format(final_path))
+        return
+    with open(final_path, 'r') as f:
         lines = f.readlines()
 
     for line in lines:
@@ -17,4 +21,10 @@ def main(base_dir: str):
 
 if __name__ == '__main__':
     base_dir = os.path.dirname(sys.argv[0])
-    main(base_dir)
+    task_txt_file = None
+    try:
+        task_txt_file = sys.argv[1]
+    except:
+        print('Need task.txt file name')
+        exit(-1)
+    main(base_dir, task_txt_file)
